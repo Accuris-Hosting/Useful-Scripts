6c865cea37ca5c924bcd5978c62f51437f16b252
Peter Potvin <potvinp@users.noreply.github.com>
2021-01-25 13:11:17 -0500

    Create debian10-bird2-install.sh

---
+++ debian10-bird2-install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+# fail in a sane manner
+set -eo pipefail
+
+if [ "$(whoami)" != "root" ]; then
+    SUDO=sudo
+fi
+
+${SUDO} curl -sSL -o /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+${SUDO} sh -c 'echo "deb http://bird.network.cz/debian/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/bird.list'
+${SUDO} apt-get update
+${SUDO} apt-get -y install bird2
