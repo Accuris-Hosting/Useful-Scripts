2a72d52429f517c2ddbc199f2d6791050b508e05
Peter Potvin <peter.potvin@peterpotvinconsulting.ca>
2021-03-25 11:41:21 -0400

    Update debian10-bird2-install.sh

--- debian10-bird2-install.sh
+++ debian10-bird2-install.sh
@@ -10,3 +10,5 @@ ${SUDO} curl -sSL -o /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/ph
 ${SUDO} sh -c 'echo "deb http://bird.network.cz/debian/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/bird.list'
 ${SUDO} apt-get update
 ${SUDO} apt-get -y install bird2
+${SUDO} systemctl enable bird
+${SUDO} systemctl start bird
