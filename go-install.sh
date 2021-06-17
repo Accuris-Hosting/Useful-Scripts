ae0f8b612cbe92cb449404624611b429c1c7f2fc
Peter Potvin <peter.potvin@peterpotvinconsulting.ca>
2021-06-16 21:30:07 -0400

    Create go-install.sh

---
+++ go-install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+# fail in a sane manner
+set -eo pipefail
+
+if [ "$(whoami)" != "root" ]; then
+    SUDO=sudo
+fi
+
+echo "Changing directory to /tmp"
+cd /tmp
+echo "Downloading go binary"
+wget https://golang.org/dl/go1.16.5.linux-amd64.tar.gz
+echo "Removing previous go installations"
+${SUDO} rm -rf /usr/local/go
+echo "Extracting go binary"
+${SUDO} tar -C /usr/local -xzf /tmp/go1.16.5.linux-amd64.tar.gz
+echo "Adding /usr/local/go/bin to the PATH environment variable"
+${SUDO} sh -c 'echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile'
+echo "Removing tarball"
+cd /tmp
+rm -rf /tmp/go1.16.5.linux-amd64.tar.gz
+echo "Go installation completed successfully!"
+exit
