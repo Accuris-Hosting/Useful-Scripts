e2cc84493aa1b94dbf3f31bd0854a9aad8c3eecf
Peter Potvin <peter.potvin@peterpotvinconsulting.ca>
2021-07-20 14:13:22 -0400

    Update go to v1.16.6

--- go-install.sh
+++ go-install.sh
@@ -9,15 +9,15 @@ fi
 echo "Changing directory to /tmp"
 cd /tmp
 echo "Downloading go binary"
-wget https://golang.org/dl/go1.16.5.linux-amd64.tar.gz
+wget https://golang.org/dl/go1.16.6.linux-amd64.tar.gz
 echo "Removing previous go installations"
 ${SUDO} rm -rf /usr/local/go
 echo "Extracting go binary"
-${SUDO} tar -C /usr/local -xzf /tmp/go1.16.5.linux-amd64.tar.gz
+${SUDO} tar -C /usr/local -xzf /tmp/go1.16.6.linux-amd64.tar.gz
 echo "Adding /usr/local/go/bin to the PATH environment variable"
 ${SUDO} sh -c 'echo "export PATH=$PATH:/usr/local/go/bin" >> /etc/profile'
 echo "Removing tarball"
 cd /tmp
-rm -rf /tmp/go1.16.5.linux-amd64.tar.gz
+rm -rf /tmp/go1.16.6.linux-amd64.tar.gz
 echo "Go installation completed successfully!"
 exit
